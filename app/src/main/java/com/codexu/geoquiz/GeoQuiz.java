9acfb437ea7bd0577ac93bad325e67834b0f56bf
xjbhenry <xjbhenry@gmail.com>
2014-09-17 12:08:46 -0400

    Initial commit

---
+++ app/src/main/java/com/codexu/geoquiz/GeoQuiz.java
@@ -0,0 +1,167 @@
+package com.codexu.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class GeoQuiz extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true),
+    };
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        Log.d(TAG, "Updating question text for question #" + mCurrentIndex,
+                new Exception());
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.toast_correct;
+        } else {
+            messageResId = R.string.toast_incorrect;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_geo_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        //int question = mQuestionBank[mCurrentIndex].getQuestion();
+        //mQuestionTextView.setText(question);
+
+        mTrueButton = (Button)findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(GeoQuiz.this, R.string.toast_correct, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        });
+        mFalseButton = (Button)findViewById(R.id.true_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(GeoQuiz.this, R.string.toast_incorrect, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
+            }
+        });
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                //int question = mQuestionBank[mCurrentIndex].getQuestion();
+               // mQuestionTextView.setText(question);
+                updateQuestion();
+            }
+        });
+        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentIndex == 0) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                } else {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                }
+                updateQuestion();
+            }
+        });
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        updateQuestion();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.geo_quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void  onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+}
---
+++ app/src/main/java/com/codexu/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.codexu.geoquiz;
+
+/**
+ * Created by Henry on 9/12/2014.
+ */
+public class TrueFalse {
+
+    private int mQuestion;
+    private boolean mTrueQuestion;
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
